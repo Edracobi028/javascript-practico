51bac6e3d3fa7b7f8b8dee4566b7434660811091
Eduardo Razo <esse_rios_e@hotmail.com>
2023-04-19 05:31:59 -0600

    Leyendo HTML desde JavaScript 11/29

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello world')
+/* Si queremos seleccionar una etiqueta la declaramos */
+
+const h1 = document.querySelector('h1'); /* etiqueta h1 */
+const p = document.querySelector('p'); /* etiqueta p */
+const parrafito = document.querySelector('.parrafito'); /* clase */
+const pid = document.querySelector('#pid');
+const input = document.querySelector('input');
+
+console.log(input.value);
+/* 11:04 */
+/* pintar todos en un console.log */
+console.log({
+    h1,
+    p,
+    parrafito,
+    pid,
+    input 
+});
