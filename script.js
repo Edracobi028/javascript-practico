ac4c4bbf09f011118344ed2b0ae37c3401772305
Eduardo Razo <esse_rios_e@hotmail.com>
2023-04-19 06:16:31 -0600

    Escribiendo HTML desde JavaScript 12/29

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 
 const h1 = document.querySelector('h1'); /* etiqueta h1 */
 const p = document.querySelector('p'); /* etiqueta p */
-const parrafito = document.querySelector('.parrafito'); /* clase */
-const pid = document.querySelector('#pid');
+const parrafito = document.getElementsByClassName('parrafito'); /* clase */
+const pid = document.getElementById('pid');
 const input = document.querySelector('input');
 
 console.log(input.value);
@@ -16,3 +16,32 @@ console.log({
     pid,
     input 
 });
+/* Cambiar el contendido desde JS */
+h1.innerHTML = 'World <br> heavyweigth Champion';  
+h1.innerText = 'World <br> heavyweigth Champion';  
+
+/* Consultar atributo */
+//console.log(h1.getAttribute('class'));
+/* editar atributo */
+//h1.setAttribute('class','rojo');
+
+/* Agregar un atributo*/
+h1.classList.add('rojo');
+h1.classList.remove('verde');
+
+/* Modificar un valor de manera mas directa */
+input.value = "456";
+
+/* #1 Crear un elemento desde cero */
+const img = document.createElement('img');
+/* #2 Agregar atributo */
+img.setAttribute('src', 'https://images.hola.com/imagenes/mascotas/20201104178485/consejos-gatos-para-principiantes/0-884-859/gatito-m.jpg?tx=w_680' );
+console.log(img);
+/* borramos el contenido, no el elemento */
+pid.innerHTML = "";
+
+/* #3 insertar en un elemento existente */
+pid.appendChild(img);
+
+
+
